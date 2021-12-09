508b51bbe3d1aa113ac58a52426a38ebd193c882
Nick Chan <nickncchan@gmail.com>
2021-12-08 17:55:31 -0800

    commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const heroesData = require("./databases/heroes.json");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("bye world");
+});
+
+app.get("/heroes", (req, res) => {
+    res.json(heroesData);
 });
 
 app.listen(port, () => {
