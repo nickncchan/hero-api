1c1ef107c2299682f8450a8dae076bb15ed45909
Nick Chan <nickncchan@gmail.com>
2021-12-09 22:21:58 -0800

    commit

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("bye world");
+    res.send("i hate superheroes");
 });
 
 app.get("/heroes", (req, res) => {
     res.json(heroesData);
+});
+
+app.get("/heroes/:id", (req, res) => {
+    const heroID = req.params['id'];
+    const hero = heroesData.heroes[heroID - 1];
+    if (hero === undefined) {
+        res.send('you\'ve gone too far');
+    }
+    res.json(hero);
 });
 
 app.listen(port, () => {
