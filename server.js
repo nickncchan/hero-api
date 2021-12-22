54c303caec4e31fc2e4d8ef2d34ac57b3b1248ca
Nick Chan <nickncchan@gmail.com>
2021-12-21 16:48:15 -0800

    commit

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 /* Global Variables*/
 const port = 3000;
 
+
+/* Endpoints/Routes */
 app.get("/", (req, res) => {
     res.send("i hate superheroes");
 });
 
-/* Endpoints/Routes */
 app.get("/heroes", (req, res) => {
     res.json(heroesData);
 });
@@ -30,6 +31,15 @@ app.get("/heroes/:id", (req, res) => {
     }
     res.json(hero);
 });
+
+app.post("/heroes", (req, res) => {
+    const newHero = req.body;
+    newHero.id = heroesData.heroes.length + 1;
+    heroesData.heroes.push(newHero);
+    console.log(heroesData);
+    res.json(newHero);
+});
+
 
 /* Server Listening on Port */
 app.listen(port, () => {
